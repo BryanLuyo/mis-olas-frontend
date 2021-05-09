bf27c5983ddfb77221ac752095c69704bb5efb89
bluyo <bryan.luyo@teayudo.pe>
2021-05-09 12:07:06 -0500

    V1

---
+++ static/js/script.js
@@ -0,0 +1,65 @@
+
+$('.owl-video').owlCarousel({
+  loop:true,  
+  nav:false,   
+  responsive:{
+      0:{
+          items:1
+      },
+      600:{
+          items:2
+      }, 
+      1000:{
+          items:3
+      }
+  }
+})
+
+
+$('.owl-evento').owlCarousel({
+  loop:true,  
+  nav:false,   
+  responsive:{
+      0:{
+          items:1
+      },
+      600:{
+          items:2
+      }, 
+      1000:{
+          items:2
+      }
+  }
+})
+
+$('.owl-playa').owlCarousel({
+  loop:true,  
+  nav:true,
+  margin:20,  
+  autoplay:true,
+  dots:false,          
+  responsive:{
+      0:{
+          items:1
+      },
+      600:{
+          items:2
+      }, 
+      1000:{
+          items:4
+      }
+  }
+})
+
+
+
+  AOS.init();
+
+  $("#mouse-scroll").click(function(){
+    // alert("hola");
+    var x=  $('.playas').offset().top + -0;
+    $('html, body').animate({
+      scrollTop: x
+    }, 1500);
+  });
+  
